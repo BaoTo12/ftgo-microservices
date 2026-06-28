b941020d93276ceb878803b06f8001d62765652c
ChiBao <baotochi87@gmail.com>
2026-06-28 13:58:59 +0700

    Init project

---
+++ src/test/java/com/chibao/foodtogosystem/FoodToGoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chibao.foodtogosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodToGoSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
